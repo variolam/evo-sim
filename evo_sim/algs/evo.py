3b11bc4fe29d88f163fd026ba90b0edff85aad1b
Michael Variola <michael.variola@gmail.com>
2022-12-25 19:06:04 +0100

    Basic layout of evo

--- evo_sim/algs/evo.py
+++ evo_sim/algs/evo.py
@@ -1,11 +1,24 @@
+import numpy as np
+
 from evo_sim.algs.repr import Individual
 
 
 class GeneticAlgorithm:
 
-    def __init__(self, population_size: int) -> None:
+    def __init__(
+        self,
+        population_size: int,
+        max_x: int = 100,
+        max_y: int = 100,
+    ) -> None:
         self.population_size = population_size
         self._generation = 0
+        self.population = [
+            Individual(
+                x_pos=np.random.uniform(0, high=max_x),
+                y_pos=np.random.uniform(0, high=max_y),
+            ) for _ in range(population_size)
+        ]
 
     def __call__(self, *args, **kwds) -> list[Individual]:
-        pass
+        return self.population
--- evo_sim/algs/repr.py
+++ evo_sim/algs/repr.py
@@ -7,5 +7,6 @@ import dataclasses
 class Individual:
     x_pos: float
     y_pos: float
+    radius: int = 5
     colour: tuple = (0, 0, 255)
     text: str = ''
