21c2dde6fa6a19c886d4e61393fcc1c08e379a89
Michael Variola <michael.variola@gmail.com>
2022-12-25 11:45:16 +0100

    Added dev dependencies and whitespace fixes

--- ir_sim/__main__.py
+++ ir_sim/__main__.py
@@ -1,24 +1,26 @@
 from ir_sim import functions
 import numpy as np
-import pygame, sys
-from pygame.locals import *
+import pygame
+from pygame import locals
+import sys
 pygame.init()
- 
+
 # Colours
-BACKGROUND = (144, 238, 144)
- 
+BACKGROUND_C = (144, 238, 144)
+LINE_C = (74, 87, 51)
+
 # Game Setup
 FPS = 60
 fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
- 
+
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('IR-Sim')
- 
+
 
 # The main function that controls the game
-def main () :
+def main():
     looping = True
     hill_x = np.arange(WINDOW_WIDTH)
     hill_y = functions.hill(
@@ -30,14 +32,14 @@ def main () :
     points = list(zip(hill_x, hill_y))
     print(f"Max Point: {max(hill_y)}")
     print(f"Min Point: {min(hill_y)}")
-    WINDOW.fill(BACKGROUND)
-    pygame.draw.lines(WINDOW, [74, 87, 51], False, points, 3)
+    WINDOW.fill(BACKGROUND_C)
+    pygame.draw.lines(WINDOW, LINE_C, False, points, 3)
 
     # The main game loop
-    while looping :
+    while looping:
         # Get inputs
-        for event in pygame.event.get() :
-            if event.type == QUIT :
+        for event in pygame.event.get():
+            if event.type == locals.QUIT:
                 pygame.quit()
                 sys.exit()
 
@@ -47,7 +49,7 @@ def main () :
         # Render elements of the game
         pygame.display.update()
         fpsClock.tick(FPS)
-    
+
 
 if __name__ == '__main__':
     main()
